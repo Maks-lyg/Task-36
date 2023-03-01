a9e0ef3196078ea87273be8727b4fda6b4bb38f9
Maks <maks-lug333@yandex.ru>
2023-03-01 21:36:36 +0900

    Не работает

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Введите размер");
+int size = int.Parse(Console.RaedLine());
+int s = Chetnie(sum);
+Console.Write($"Ответ - {s}");
+
+int[] Array(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i< size; i++)
+    {
+        array[i] = new Random().Next(10,100);
+    }
+    return array;
+}
+
+int Chetnie(int evNumber)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i+=2)
+    {
+        sum += array[i]; // изначально было -> sum += i;
+    }
+    return sum;
+}
